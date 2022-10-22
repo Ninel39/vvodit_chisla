46a37a7070f65620e60c845c8a2e5d81648011e0
User <User@WIN-5U3F60ENH1L>
2022-10-23 02:20:00 +0300

    Добавьте файлы проекта.

---
+++ vvodit_chisla/vvodit_chisla.cpp
@@ -0,0 +1,22 @@
+﻿// vvodit_chisla.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(0, "");
+
+    int a, b;
+    a = 1; b = 0;
+    while (a != 0)
+    {
+        cin >> a;
+        b = b + a;
+
+    }
+    cout << b << endl;
+
+}
+
